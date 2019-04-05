4b927a611718c3c47ed27e08cf7f5e9e13eaff53
Vicsante Tangpos Aseniero <vaseniero@gmail.com>
2019-04-05 13:10:52 +0800

    changes in Users Migration Table

--- Laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ Laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique('UserEmailUnique');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('mobile_number')->unique()->nullable();
+            $table->string('mobile_number')->unique('UserMobileUnique')->nullable();
             $table->date('birthday')->nullable();
             $table->char('sex', 1)->nullable();
             $table->boolean('verified')->default(false);
